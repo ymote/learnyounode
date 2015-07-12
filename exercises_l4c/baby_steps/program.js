756f861eec8e63a20fc2bae738886fe3e3be646b
Yue Chen <dspfac@gmail.com>
2015-07-12 10:09:18 +0000

    commit exercise content by Live4Code

--- exercises_l4c/baby_steps/program.js
+++ exercises_l4c/baby_steps/program.js
@@ -2,5 +2,5 @@ var result = 0
 
 for (var i = 2; i < process.argv.length; i++)
   // enter your code below
-result=result+process.argv[i];
+result=result+Number(process.argv[i]);
 console.log(result);
